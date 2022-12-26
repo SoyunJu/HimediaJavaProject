6f48c9197516a72628fe799a3377acb704896627
soyun <swell1402@naver.com>
2022-12-26 11:00:28 +0900

    로컬 및 원격 저장소에 반영

--- src/mybank/Account.java
+++ src/mybank/Account.java
@@ -27,6 +27,7 @@ public class Account {
 		}
 		if (amount < 0) {
 			System.out.println("마이너스 금액은 예금할 수 없습니다.");
+			System.out.println("깃허브에 반영할 내용");
 			return;
 		}
 		this.balance += amount;
@@ -36,7 +37,6 @@ public class Account {
 	public void withdraw(String accountNo, int amount) {
 		if (!this.accountNo.equals(accountNo)) {
 			System.out.println("계좌번호가 일치하지 않습니다.");
-			System.out.println("깃 첫번째 수정");
 			return;
 		}
 		if (amount < 0) {
